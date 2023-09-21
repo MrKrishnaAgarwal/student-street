5187a3057a4a1286acbef07dd06e6495dbaa8cce
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2023-09-21 12:44:54 +0530

    changed

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,15 @@
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-        Made for students by students
-        Copyright © 2023 | Krishna Agarwal | All Rights Reserved
-        <img src="https://media.discordapp.net/attachments/1048479953592795156/1154312414494478376/student_street.jpg?width=802&height=580" alt="Student Street Logo" className={styles.logo} />
-      </footer>
-    </>
-  )
+    <footer className={styles.footer}>
+      <img
+        src="https://media.discordapp.net/attachments/1048479953592795156/1154312414494478376/student_street.jpg?width=802&height=580"
+        alt="Student Street Logo"
+        className={styles.logo}
+      />
+      <p>Made for students by students</p>
+      <p>Copyright © 2023 | Krishna Agarwal | All Rights Reserved</p>
+    </footer>
+  );
 }

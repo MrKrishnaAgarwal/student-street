12ccd6edc4574d4e34bb03b4bf738e67d4f1394b
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2023-09-21 12:42:00 +0530

    changed copyright notice

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Made for students by students
-        Copyright © 2022. All Rights Reserved.
+        Copyright © 2023 | Krishna Agarwal | All Rights Reserved
         <img src="https://media.discordapp.net/attachments/1048479953592795156/1154312414494478376/student_street.jpg?width=802&height=580" alt="Student Street Logo" className={styles.logo} />
       </footer>
     </>

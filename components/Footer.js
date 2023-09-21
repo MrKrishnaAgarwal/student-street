a88faa560a41a926e9e231e7b70c024822de4c4f
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2023-09-21 12:38:45 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer className={styles.footer}>
         Made for students by students
         Copyright © 2022. All Rights Reserved.
-          <br>
+          <br> </br>
           <img src="https://media.discordapp.net/attachments/1048479953592795156/1154312414494478376/student_street.jpg?width=802&height=580" alt="Student Street Logo" className={styles.logo} />
       </footer>
     </>
